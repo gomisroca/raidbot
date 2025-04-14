84e29a031849dedd500dc551832d3baa0fb714ec
asfalfa <glkin9@gmail.com>
2025-04-14 14:45:14 +0200

    init

---
+++ src/commands.js
@@ -0,0 +1,14 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const DAYS_COMMAND = {
+  name: 'days',
+  description: 'Create a poll to decide which days we raid.',
+};
+
+export const TIMES_COMMAND = {
+  name: 'times',
+  description: 'Create a poll to decide at which time we start raid.',
+};
